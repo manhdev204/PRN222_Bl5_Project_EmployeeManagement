7393aeb4413822dc7099bc43f52fd97ca09bd7d4
manhntdev <manhnguyenlhpnd@gmail.com>
2025-08-25 21:52:10 +0700

    Change fault role when register

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -64,6 +64,11 @@ namespace PRN222_BL5_Project_EmployeeManagement.Controllers
             HttpContext.Session.SetString(SessionKeyUsername, user.Username);
             HttpContext.Session.SetString(SessionKeyRole, user.RoleId.ToString());
 
+            if (user.RoleId == 1)
+            {
+                return RedirectToAction("Index", "AdminAccounts");
+            }
+
             return RedirectToAction("Index", "Home");
         }
 
@@ -103,7 +108,7 @@ namespace PRN222_BL5_Project_EmployeeManagement.Controllers
             var defaultRoleId = _db.Roles.OrderBy(r => r.RoleId).Select(r => r.RoleId).FirstOrDefault();
             if (defaultRoleId == 0)
             {
-                defaultRoleId = 1;
+                defaultRoleId = 3;
             }
 
             var account = new Account
